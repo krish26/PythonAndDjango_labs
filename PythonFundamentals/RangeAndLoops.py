360e52004e7286009adbacf1def32e4b1de3c120
krishna koumudi <koumudi26@gmail.com>
2025-12-11 16:02:42 +0100

    december 11 labs

--- PythonFundamentals/RangeAndLoops.py
+++ PythonFundamentals/RangeAndLoops.py
@@ -5,7 +5,7 @@ for i in range(1,20):   #Printing Odd numbers
 sum=0
 for i in range(1,100):
     sum=sum+i
-print("Sum of 1-100 numbers is : " ,sum)  #Printing sum of 100 numbers
+print("\n Sum of 1-100 numbers is : " ,sum)  #Printing sum of 100 numbers
 
 
 num=int(input("Enter a number to get multiplication table : "))
