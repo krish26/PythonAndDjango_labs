3b404f38e87bb02870b5b2f354a0cc93947f22b6
krishna koumudi <koumudi26@gmail.com>
2026-01-05 17:13:31 +0100

    blog_initial

--- Django_learn/Idea/urls.py
+++ Django_learn/Idea/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for first project.
+URL configuration for Idea project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/' ,user_views.register, name='register'),
+    path('', include('blog.urls')),
+    
+
 ]
